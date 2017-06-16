afa65d2fd56c620a22bd0fff6e33cbb21c094b1b
apklemon <hazim22291@hotmail.com>
2017-06-16 08:52:06 -0500

    Initial commit

---
+++ OpenALPR_CS/FpsCounter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace OpenALPR_CS
+{
+    class FpsCounter
+    {
+        private long[] elapsedArray;
+        private int currentIndex = 0;
+        private Stopwatch sw = new Stopwatch(); 
+        
+        public FpsCounter()
+        {
+            elapsedArray = new long[5];
+        }
+
+        public FpsCounter(int bufferSize)
+        {
+            elapsedArray = new long[bufferSize];
+        }
+
+        public void Restart()
+        {
+            sw.Restart();
+        }
+
+        public void Stop()
+        {
+            sw.Stop();
+            elapsedArray[GetNextIndex()] = sw.ElapsedMilliseconds;
+        }
+
+        private int GetNextIndex()
+        {
+            currentIndex++;
+            if(currentIndex >= elapsedArray.Length)
+            {
+                currentIndex = 0;
+            }
+            return currentIndex;
+        }
+
+        public double GetFPS()
+        {
+            int len = elapsedArray.Length;
+            long accum = 0;
+            for(int i=0; i < len; i++)
+            {
+                accum += elapsedArray[i];
+            }
+            double mspf = ((double)accum) / ((double)len);
+            double fps = 1000.0 / mspf;
+            return fps;
+        }
+
+    }
+}
---
+++ OpenALPR_CS/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using openalprnet;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using System.Drawing;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+
+namespace OpenALPR_CS
+{
+    class Program
+    {
+        private static AlprNet alpr;
+        private static Capture cap;
+        private static Image<Bgr, byte> frame = new Image<Bgr, byte>(1,1);
+        private static Mat frameInput;
+        private static int frameIndex = 0;
+        private static FpsCounter fps = new FpsCounter(10);
+        private static string plateOutputDirectory = @"C:\opencv\data\plates";
+
+        static void Main(string[] args)
+        {
+            if(!Directory.Exists(plateOutputDirectory))
+            {
+                Directory.CreateDirectory(plateOutputDirectory);
+            }
+
+            alpr = new AlprNet("us", @"C:\openalpr_64\openalpr.conf", @"C:\openalpr_64\runtime_data");
+            if (!alpr.IsLoaded())
+            {
+                Console.WriteLine("OpenAlpr failed to load!");
+                return;
+            }
+            // Optionally apply pattern matching for a particular region
+            alpr.DefaultRegion = "ny";
+            alpr.TopN = 1;
+
+
+            string imgPath = @"C:\opencv\data\tag.jpg";
+            //FindPlates(new Image<Bgr, byte>(imgPath));
+            //Thread.Sleep(2000);
+            cap = new Capture(@"c:\opencv\data\sidewalk2.mp4");
+            //cap = new Capture();
+            int skipFrame = 0;
+            int skipFPS = 0;
+
+            while (true)
+            {
+                fps.Restart();
+                frameInput = cap.QueryFrame();
+                skipFrame++;
+                skipFPS++;
+                if (skipFrame % 30 == 0)
+                {
+                    if (frameInput != null)
+                    {
+                        frameIndex++;
+                        frame = frameInput.ToImage<Bgr, byte>();
+                        frame = frame.Rotate(180, new Bgr(0, 0, 0));
+                        //frame = frame.Resize(0.5, Emgu.CV.CvEnum.Inter.Area);
+                        FindPlates(frame);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No more frames.");
+                        break;
+                    }
+                }
+                fps.Stop();
+                if(skipFPS % 100 == 0)
+                {
+                    Console.WriteLine(fps.GetFPS().ToString("0.00") + " fps");
+                }
+            }
+            cap.Stop();
+            Console.WriteLine("No more capture.");
+            CvInvoke.WaitKey();
+
+        }
+
+        private static void ImageGrabbed(object sender, EventArgs e)
+        {
+            cap.Retrieve(frame);
+            FindPlates(frame);
+        }
+
+        private static void FindPlates(Image<Bgr, byte> input)
+        {
+            Rectangle ROI = new Rectangle(0, (int)(input.Height * 0.2), input.Width, (int)(input.Height * 0.6));
+            frame.Draw(ROI, new Bgr(0, 255, 0), 1);
+            var results = alpr.Recognize(input.Bitmap, new List<Rectangle>(){ROI});
+            
+
+            //if(results.Plates.Count == 0 ) Console.WriteLine("No plates found.");
+            foreach (var result in results.Plates)
+            {
+                // Save plate region to file
+                frame.ROI = GetBoundingRectangle(result.PlatePoints);
+                frame.Save(Path.Combine(plateOutputDirectory, result.BestPlate.Characters + "_" + DateTime.Now.ToString("yyyymmdd_HHMMssfff") + ".png"));
+                frame.ROI = Rectangle.Empty;
+
+                frame.Draw(result.PlatePoints.ToArray(), new Bgr(0, 0, 255), 2);
+                frame.Draw(result.BestPlate.Characters, result.PlatePoints[0], Emgu.CV.CvEnum.FontFace.HersheySimplex, 1, new Bgr(0, 0, 0), 5);
+                frame.Draw(result.BestPlate.Characters, result.PlatePoints[0], Emgu.CV.CvEnum.FontFace.HersheySimplex, 1, new Bgr(0, 0, 255), 3);
+                Console.WriteLine("[" + frameIndex.ToString() + "] Plate: " + result.BestPlate.Characters + " (" + result.BestPlate.OverallConfidence.ToString("0.") + "%) " + " T:" + result.ProcessingTimeMs.ToString("0.00") );
+                
+                //Console.WriteLine("Plate {0}: {1} result(s)", result.PlateIndex, result.TopNPlates.Count);
+                //Console.WriteLine("  Processing Time: {0} msec(s)", result.ProcessingTimeMs);
+                //foreach (var plate in result.TopNPlates)
+                //{
+                //    Console.WriteLine("  - {0}\t Confidence: {1}\tMatches Template: {2}", plate.Characters,
+                //                      plate.OverallConfidence, plate.MatchesTemplate);
+                //}
+            }
+
+            CvInvoke.Imshow("input", input);
+            CvInvoke.WaitKey(20);
+        }
+
+        private static Rectangle GetBoundingRectangle(List<Point> points)
+        {
+            int minx = Int32.MaxValue;
+            int maxx = Int32.MinValue;
+            int miny = Int32.MaxValue;
+            int maxy = Int32.MinValue;
+
+            foreach(var point in points)
+            {
+                minx = Math.Min(minx, point.X);
+                maxx = Math.Max(maxx, point.X);
+                miny = Math.Min(miny, point.Y);
+                maxy = Math.Max(maxy, point.Y);
+            }
+            var output = new Rectangle(minx, miny, maxx - minx, maxy - miny);
+            return output;
+        }
+    }
+}
