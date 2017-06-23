0ae311e757994c85b0a37b74b12be7d6243428bc
apklemon <hazim22291@hotmail.com>
2017-06-23 09:44:38 -0500

    added ProducerConsumer class

---
+++ EmugCVSandbox/ProducerConsumer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace EmugCVSandbox
+{
+    class ProducerConsumer<T> : IDisposable where T : class
+    {
+        object locker = new object();
+        Thread[] workers;
+        Queue<T> taskQ = new Queue<T>();
+        Action<T> _consumeAction;
+
+        public ProducerConsumer(int workerCount, Action<T> consumeAction)
+        {
+            workers = new Thread[workerCount];
+            _consumeAction = consumeAction;
+            // Create and start a separate thread for each worker
+            for (int i = 0; i < workerCount; i++)
+                (workers[i] = new Thread(Consume)).Start();
+        }
+
+        public void Dispose()
+        {
+            // Enqueue one null task per worker to make each exit.
+            foreach (Thread worker in workers) EnqueueTask(null);
+            foreach (Thread worker in workers) worker.Join();
+        }
+
+        public void EnqueueTask(T task)
+        {
+            lock (locker)
+            {
+                taskQ.Enqueue(task);
+                Console.WriteLine(task.ToString() + " produced.");
+                Monitor.PulseAll(locker);
+            }
+        }
+
+        void Consume()
+        {
+            while (true)
+            {
+                T task;
+                lock (locker)
+                {
+                    while (taskQ.Count == 0) Monitor.Wait(locker);
+                    task = taskQ.Dequeue();
+                }
+                if (task == null) return;         // This signals our exit
+                //Console.Write(task);
+                if (_consumeAction != null)
+                {
+                    _consumeAction.Invoke(task);
+                }
+                Thread.Sleep(100);              // Simulate time-consuming task
+            }
+        }
+    }
+}
+
